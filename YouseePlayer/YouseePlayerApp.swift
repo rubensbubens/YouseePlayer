aa587667e25ae30e9d10fba0b7b9d38e0789cd63
Serj Rubens <serj.rubens@gmail.com>
2022-10-20 12:45:32 +0200

    Initial Commit

---
+++ YouseePlayer/YouseePlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouseePlayerApp.swift
+//  YouseePlayer
+//
+//  Created by Serhii Bazarnyi on 20.10.22.
+//
+
+import SwiftUI
+
+@main
+struct YouseePlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
